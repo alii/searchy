02a17976b7c78e8e860a9351e35fb502feb7a688
H4rl <98224660+H4rldev@users.noreply.github.com>
2022-07-19 22:36:38 +0200

    forgot a comma :(

--- src/links.ts
+++ src/links.ts
@@ -20,8 +20,8 @@ export const SITES: Record<string, string | ((query: string) => string) | undefi
 	namelix: 'https://namelix.com/app/?keywords={q}',
 	namecheap: 'https://www.namecheap.com/domains/registration/results/?domain={q}',
 	googledomains: 'https://domains.google.com/registrar/search?searchTerm={q}',
-  godaddy: 'https://se.godaddy.com/domainsearch/find?checkAvail=1&domainToCheck={q}',
-  porkbun: 'https://porkbun.com/checkout/search?q={q}',
+  	godaddy: 'https://se.godaddy.com/domainsearch/find?checkAvail=1&domainToCheck={q}',
+ 	porkbun: 'https://porkbun.com/checkout/search?q={q}',
 
 	// Programming
 	git: 'https://github.com/search?q={q}',
@@ -128,8 +128,8 @@ export const SITES: Record<string, string | ((query: string) => string) | undefi
 	animixplay: 'https://animixplay.to/?q={q}&sengine=gogo',
 
 	zoro: 'https://zoro.to/search?keyword={q}',
-  mangaupdates: 'https://www.mangaupdates.com/search.html?search={q}',
- '9anime': 'https://9anime.to/search?keyword={q}&vrf=Ux9h8vwyLd',
+  	mangaupdates: 'https://www.mangaupdates.com/search.html?search={q}',
+ 	'9anime': 'https://9anime.to/search?keyword={q}&vrf=Ux9h8vwyLd',
 
 	// .new
 	figma: 'https://figma.new',
@@ -190,7 +190,7 @@ export const SITES: Record<string, string | ((query: string) => string) | undefi
 	'gnarly-repacks': 'https://www.gnarly-repacks.site/?s={q}', // Games Site. -H4rldev
 	fitgirl: 'https://fitgirl-repacks.site/?s={q}', // One of the most popular sources for repacks of games. Highly recommended. -H4rldev
 	'dodi-repacks': 'https://dodi-repacks.site/?s={q}', // A lot of dodi's content is the same as fitgirl's. -H4rldev
-	kaoskrew: 'https://kaoskrew.org/search.php?keywords=yeah&fid%5B0%5D=13' // Torrenting forum for games. Many known crackers post their content there. -H4rldev
+	kaoskrew: 'https://kaoskrew.org/search.php?keywords=yeah&fid%5B0%5D=13', // Torrenting forum for games. Many known crackers post their content there. -H4rldev
 
 	// Twitter utils
 	tweet: 'https://twitter.com/intent/tweet?text={q}',
